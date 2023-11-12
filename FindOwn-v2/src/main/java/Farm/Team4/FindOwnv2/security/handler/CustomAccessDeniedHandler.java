d8851441c7a74e3a3fd0d68062bbba8759335a26
dlawjddn <jwl5015@naver.com>
2023-11-13 01:38:46 +0900

    Feat: access denied handler 추가

---
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package Farm.Team4.FindOwnv2.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.info("access denied handler 진입: 권한이 없는 사용자의 접근");
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "권한이 없는 사용자 입니다.");
+    }
+}
