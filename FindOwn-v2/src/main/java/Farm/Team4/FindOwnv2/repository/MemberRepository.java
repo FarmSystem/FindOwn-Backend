debf7ccfd762e0d3a6c0a02250610858bf23bd63
dlawjddn <jwl5015@naver.com>
2023-11-14 00:40:30 +0900

    Feat: memberRepository 생성

---
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package Farm.Team4.FindOwnv2.repository;
+
+import Farm.Team4.FindOwnv2.domain.platform.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+}
