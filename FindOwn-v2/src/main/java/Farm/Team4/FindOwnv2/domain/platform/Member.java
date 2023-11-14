ac92db6b158c5accada75be425b7cefc7578c978
dlawjddn <jwl5015@naver.com>
2023-11-14 22:07:18 +0900

    Feat: member constructor 생성

--- FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/domain/platform/Member.java
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/domain/platform/Member.java
@@ -3,6 +3,7 @@ package Farm.Team4.FindOwnv2.domain.platform;
 import Farm.Team4.FindOwnv2.domain.community.board.Post;
 import Farm.Team4.FindOwnv2.domain.community.issue.Scrap;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,8 +21,8 @@ public class Member {
     private Long Id;
     private String username;
     private String password;
-    private String nickname;
-    private String phoneNumber;
+    private String korName;
+    private String email;
     private LocalDateTime register;
     private String role;
     @OneToMany(mappedBy = "ownMember", cascade = CascadeType.ALL)
@@ -32,4 +33,13 @@ public class Member {
     private List<Post> myPosts = new ArrayList<>();
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<Scrap> myScraps = new ArrayList<>();
+
+    public Member(String username, String password, String korName, String email) {
+        this.username = username;
+        this.password = password;
+        this.korName = korName;
+        this.email = email;
+        this.register = LocalDateTime.now();
+        this.role = "USER";
+    }
 }
