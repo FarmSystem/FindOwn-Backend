e4a86e25309f95d1ff2d96056122c38cd7794209
dlawjddn <jwl5015@naver.com>
2023-11-20 02:40:45 +0900

    Feat: 회원 id, password 변경 service 로직 생성

--- FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/service/member/MemberService.java
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/service/member/MemberService.java
@@ -2,17 +2,22 @@ package Farm.Team4.FindOwnv2.service.member;
 
 import Farm.Team4.FindOwnv2.domain.platform.Member;
 import Farm.Team4.FindOwnv2.dto.auth.response.ResultPasswordDTO;
+import Farm.Team4.FindOwnv2.dto.member.request.ChangeMemberIdDTO;
+import Farm.Team4.FindOwnv2.dto.member.request.ChangeMemberPasswordDTO;
 import Farm.Team4.FindOwnv2.dto.member.request.SaveMemberDTO;
 import Farm.Team4.FindOwnv2.dto.member.response.CheckIdDuplicatedDTO;
 import Farm.Team4.FindOwnv2.exception.CustomErrorCode;
 import Farm.Team4.FindOwnv2.exception.FindOwnException;
 import Farm.Team4.FindOwnv2.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
@@ -35,12 +40,37 @@ public class MemberService {
         return memberRepository.findByUsername(username)
                 .orElseThrow(() -> new FindOwnException(CustomErrorCode.NOT_FOUND_MEMBER));
     }
-    public CheckIdDuplicatedDTO idDuplicated(String username){
+    public CheckIdDuplicatedDTO isDuplicatedId(String username){
         return new CheckIdDuplicatedDTO(!memberRepository.existsByUsername(username));
     }
     public ResultPasswordDTO isMatchPassword(String rawPassword){
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        Member member = findByUsername(username);
+        Member member = getMember();
         return new ResultPasswordDTO(passwordEncoder.matches(rawPassword, member.getPassword()));
+    }
+    @Transactional
+    public void changeUsername(ChangeMemberIdDTO changeMemberIdDTO) {
+        Member member = findByUsername(changeMemberIdDTO.getOriginMemberId());
+        if (!member.getUsername().equals(changeMemberIdDTO.getOriginMemberId()))
+            throw new FindOwnException(CustomErrorCode.NOT_FOUND_MEMBER);
+        member.changeUsername(changeMemberIdDTO.getNewMemberId());
+        log.info("회원 아이디 변경 완료");
+    }
+    @Transactional
+    public void changePassword(ChangeMemberPasswordDTO changeMemberPasswordDTO) {
+        Member member = getMember();
+        if (!passwordEncoder.matches(changeMemberPasswordDTO.getOriginMemberPw(), member.getPassword()))
+            throw new FindOwnException(CustomErrorCode.WRONG_PASSWORD);
+        member.changeEncoded(passwordEncoder.encode(changeMemberPasswordDTO.getNewMemberPw()));
+        log.info("비밀번호 변경 완료, 암호화 완료");
+    }
+    @Transactional
+    public void deleteMember() {
+        Member member = getMember();
+        memberRepository.delete(member);
+        log.info("회원 삭제 완료");
+    }
+    private Member getMember(){
+        return findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+
     }
 }
