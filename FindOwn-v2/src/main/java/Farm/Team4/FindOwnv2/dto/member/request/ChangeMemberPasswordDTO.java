2fa913903fe4b9a36da342a1414b241593b49a32
dlawjddn <jwl5015@naver.com>
2023-11-20 02:40:18 +0900

    Feat: 회원 password 변경 request dto 생성

---
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/dto/member/request/ChangeMemberPasswordDTO.java
@@ -0,0 +1,9 @@
+package Farm.Team4.FindOwnv2.dto.member.request;
+
+import lombok.Data;
+
+@Data
+public class ChangeMemberPasswordDTO {
+    private String originMemberPw;
+    private String newMemberPw;
+}
