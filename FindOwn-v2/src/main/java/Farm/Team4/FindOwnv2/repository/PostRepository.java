15bfba53b30da792f291cc25bb4ec96a62d10871
dlawjddn <jwl5015@naver.com>
2023-11-20 02:34:41 +0900

    Feat: post 조회 repository 로직 생성

--- FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/repository/PostRepository.java
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package Farm.Team4.FindOwnv2.repository;
 
 import Farm.Team4.FindOwnv2.domain.community.board.Post;
+import Farm.Team4.FindOwnv2.domain.community.board.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByCreatedAtDesc();
+    List<Post> findAllByTagOrderByCreatedAtDesc(Tag tag);
+    List<Post> findAllByTitleContainingOrderByCreatedAtDesc(String title);
 }
