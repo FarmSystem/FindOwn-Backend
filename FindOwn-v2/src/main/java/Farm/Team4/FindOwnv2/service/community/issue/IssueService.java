9b59ad260e367cb0dc361a26a01f474b0b98bd30
dlawjddn <jwl5015@naver.com>
2023-11-21 01:10:10 +0900

    Feat: issue service 생성, issue 저장, 조회 로직 생성

---
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/service/community/issue/IssueService.java
@@ -0,0 +1,57 @@
+package Farm.Team4.FindOwnv2.service.community.issue;
+
+import Farm.Team4.FindOwnv2.domain.community.issue.Issue;
+import Farm.Team4.FindOwnv2.dto.community.issue.request.CreateIssueDTO;
+import Farm.Team4.FindOwnv2.dto.community.issue.response.ShowIssueDetailDTO;
+import Farm.Team4.FindOwnv2.dto.community.issue.response.ShowIssueSimpleDTO;
+import Farm.Team4.FindOwnv2.exception.CustomErrorCode;
+import Farm.Team4.FindOwnv2.exception.FindOwnException;
+import Farm.Team4.FindOwnv2.repository.IssueRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class IssueService {
+    private final IssueRepository issueRepository;
+    @Transactional
+    public void createIssue(CreateIssueDTO createIssueDTO) {
+        Issue issue = createIssueDTO.toIssue();
+        issueRepository.save(issue);
+        log.info("이슈 저장 완료");
+    }
+    public Issue findById(Long issueId){
+        return issueRepository.findById(issueId)
+                .orElseThrow(() -> new FindOwnException(CustomErrorCode.NOT_FOUND_ISSUE));
+    }
+    public ShowIssueDetailDTO showIssueDetail(Long id) {
+        Issue findIssue = findById(id);
+        findIssue.increaseView();
+        issueRepository.save(findIssue);
+        return new ShowIssueDetailDTO(
+                findIssue.getId(),
+                findIssue.getTitle(),
+                findIssue.getCategory().toString(),
+                findIssue.getContent(),
+                findIssue.getSource(),
+                findIssue.getReporter(),
+                findIssue.getViewCnt(),
+                findIssue.getScrapCnt()
+        );
+    }
+    public List<ShowIssueSimpleDTO> showIssueSimpleList() {
+        return issueRepository.findAll().stream()
+                .map(issue -> new ShowIssueSimpleDTO(
+                        issue.getTitle(),
+                        issue.getContent().substring(0,30) + "...",
+                        issue.getViewCnt(),
+                        issue.getScrapCnt(),
+                        issue.getCreatedAt()))
+                .toList();
+    }
+}
