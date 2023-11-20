9cb1cffc0a2e45542d32548baf64f976179d2d7a
dlawjddn <jwl5015@naver.com>
2023-11-21 01:18:48 +0900

    Feat: scrap controller 생성, api 생성

---
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/controller/ScrapController.java
@@ -0,0 +1,21 @@
+package Farm.Team4.FindOwnv2.controller;
+
+import Farm.Team4.FindOwnv2.service.community.scrap.ScrapService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v2/users/community/scrap")
+@RequiredArgsConstructor
+public class ScrapController {
+    private final ScrapService scrapService;
+    @PostMapping("/")
+    public void createScarp(@RequestParam Long id){
+        scrapService.createScrap(id);
+    }
+    @DeleteMapping("/")
+    public void deleteScrap(@RequestParam Long id){
+        scrapService.deleteScrap(id);
+    }
+
+}
