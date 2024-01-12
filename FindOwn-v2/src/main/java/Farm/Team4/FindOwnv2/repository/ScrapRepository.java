a0076dbae8bce48be6674830c57afdce917059a2
Lim jeong woo <dlawjddn0105@gmail.com>
2024-01-13 04:08:19 +0900

    Feat: Scrap 관련 예외처리 추가

--- FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/repository/ScrapRepository.java
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/repository/ScrapRepository.java
@@ -1,10 +1,15 @@
 package Farm.Team4.FindOwnv2.repository;
 
+import Farm.Team4.FindOwnv2.domain.community.issue.Issue;
 import Farm.Team4.FindOwnv2.domain.community.issue.Scrap;
+import Farm.Team4.FindOwnv2.domain.platform.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
     Optional<Scrap> findById(Long scrapId);
+    boolean existsByMemberAndIssue(Member member, Issue issue);
+    List<Scrap> findAllByMember(Member member);
 }
