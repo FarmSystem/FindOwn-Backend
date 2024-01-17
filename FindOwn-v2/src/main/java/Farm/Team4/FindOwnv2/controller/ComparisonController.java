6c2b20281a1e10f1deba8b50b6a873b99f55f82c
Lim jeong woo <dlawjddn0105@gmail.com>
2024-01-17 13:19:48 +0900

    Fix: 상표권 침해판단 api 수정

--- FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/controller/ComparisonController.java
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/controller/ComparisonController.java
@@ -1,6 +1,7 @@
 package Farm.Team4.FindOwnv2.controller;
 
 import Farm.Team4.FindOwnv2.dto.comparison.response.ComparisonResultDto;
+import Farm.Team4.FindOwnv2.dto.comparison.response.ComparisonTrademarkDto;
 import Farm.Team4.FindOwnv2.service.comparison.ComparisonService;
 import Farm.Team4.FindOwnv2.service.comparison.ImageService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,7 +21,7 @@ public class ComparisonController {
     private final ImageService imageService;
     private final ComparisonService comparisonService;
     @PostMapping
-    public ComparisonResultDto startComparison(@RequestParam("image")MultipartFile file) throws IOException {
+    public List<ComparisonTrademarkDto> startComparison(@RequestParam("image")MultipartFile file) throws IOException, URISyntaxException {
         log.info("file origin name = {}", file.getOriginalFilename());
         return comparisonService.confirmComparisonResult(imageService.uploadImage(file));
     }
