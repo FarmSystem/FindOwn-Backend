3baf17519b27bb7f12c8a185cb1e9e9eb3330cfd
Lim jeong woo <dlawjddn0105@gmail.com>
2024-01-18 21:35:01 +0900

    Feat: Comparison 저장 서비스 로직 생성

---
+++ FindOwn-v2/src/main/java/Farm/Team4/FindOwnv2/repository/ComparisonRepository.java
@@ -0,0 +1,7 @@
+package Farm.Team4.FindOwnv2.repository;
+
+import Farm.Team4.FindOwnv2.domain.platform.Comparison;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComparisonRepository extends JpaRepository<Comparison, Long> {
+}
